827f504df1cf7be21a5d86462205e159f463a3e5
fa01 <rfahmed01@gmail.com>
2018-11-16 10:54:54 -0800

    working on remove loop

---
+++ linkedListProblems/removeLoop.py
@@ -0,0 +1,6 @@
+# Write a function that checks whether a given Linked List contains loop and if loop is present then removes the loop and returns true. 
+# And if the list doesn’t contain loop then returns false. 
+
+
+def removeLoop(list):
+  print('removeLoop')
