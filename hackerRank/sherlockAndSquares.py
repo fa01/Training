4e19f82b93dc8f8e2ba388a8f4e60bc4e1943403
fa01 <rfahmed01@gmail.com>
2018-11-21 08:41:11 -0800

    finished sherlock and sqaures

--- hackerRank/sherlockAndSquares.py
+++ hackerRank/sherlockAndSquares.py
@@ -1,12 +1,16 @@
 # Complete the squares function below.
 def squares(a, b):
     result = 0
-    for number in range(a, b + 1):
-        print('number = ', number)
-        temp = math.sqrt(number)
-        print('temp = ', round(temp, 1))
-        print('temp**2 = ', temp**2)
-        if(temp**2 == number):
-            print('this number is a square within range = ', number)
-            result += 1
-    return result
+    return math.floor(math.sqrt(b)) - math.ceil(math.sqrt(a)) + 1
+    # for number in range(a, b + 1):
+    #     print('number = ', number)
+    #     temp = math.sqrt(number)
+    #     if(number % temp == 0):
+    #         result += 1
+    # print('temp = ', temp)
+    # print('ROUND temp**2 = ', round(temp, 2))
+    # rounded = round(temp, 2)
+    # if(rounded**2 == number):
+    #     print('number IS square = ', number)
+    #     result += 1
+    # return result
